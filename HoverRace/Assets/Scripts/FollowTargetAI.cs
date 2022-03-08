7caffbbfcceaa9f2e6027daed9c65d07b1028b2b
jsh_studios <jasdeep.heer001@gmail.com>
2022-03-08 10:33:19 +0000

    drone

---
+++ HoverRace/Assets/Scripts/FollowTargetAI.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+ 
+ 
+ 
+public class FollowTargetAI : MonoBehaviour
+{
+    public float MinDistance = 3;
+    public float MaxDistance = 1;
+    public float Speed = 3;
+    public Transform Player;
+ 
+ 
+    void Update()
+    {
+        //transform.LookAt(Player);
+        if (Vector3.Distance(transform.position, Player.position) >= MinDistance)
+        {
+            Vector3 follow = Player.position;
+           
+            follow.y = this.transform.position.y;
+            
+            this.transform.position = Vector3.MoveTowards(this.transform.position, follow, Speed * Time.deltaTime);
+        }
+    }
+}
