6fd4c8483049595b736a565cf818b62041ebfb79
jsh_studios <jasdeep.heer001@gmail.com>
2022-03-08 11:51:31 +0000

    cleanup

--- HoverRace/Assets/Scripts/playerHoverControll.cs
+++ HoverRace/Assets/Scripts/playerHoverControll.cs
@@ -2,137 +2,164 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.Serialization;
 
 public class playerHoverControll : MonoBehaviour
 {
     [Header("Hoverboard Properties")]
-    [SerializeField] private float __speed = 2;
-    [SerializeField] private float __rotSpeed = 10f;
-    [SerializeField] private float __levelForce = 150f;
+    [SerializeField] private float speed = 2;
+    [SerializeField] private float rotSpeed = 10f;
+    [SerializeField] private float levelForce = 150f;
     [SerializeField] private float hoverDist = 1;
-    [SerializeField] private float Stability = 0.3f;
-    [SerializeField] private float Stabilityspeed = 2.0f;
+    [SerializeField] private float stability = 0.3f;
+    [SerializeField] private float stabilitySpeed = 2.0f;
     [Header("Sound")]
-    [SerializeField] private float EngineAcceleration = 10f;
+    [SerializeField] private float engineAcceleration = 10f;
     [SerializeField] private float minEngineSound = .05f;
     [SerializeField] private float maxEngineSound = .6f;
-    [SerializeField] private AudioSource _shipSound;
+    [SerializeField] private AudioSource shipSound;
+    [FormerlySerializedAs("__corners")]
     [Space(5)]
     [Header("Hoverboard Points")]
-    [SerializeField] private Transform __corners;
-    [SerializeField] private Transform __Orientaion;
+    [SerializeField] private Transform corners;
+    [SerializeField] private Transform orientation;
 
 
     #region Private Vars
-        private bool groundLevel;
-        private float timeUngrounded;
-        private bool inAir = true;
-        private Rigidbody __rb;
-        private float __upForce = 1;
+        private bool _groundLevel;
+        private float _timeUngrounded;
+        private bool _inAir = true;
+        private Rigidbody _rigidbody;
+        private float _upForce = 1;
         #endregion
-    
-    void Start()
+
+    private void Awake()
+    {   
+        shipSound.Play();
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Start()
     {
-        _shipSound.Play();  
-        __rb = GetComponent<Rigidbody>();
-        __corners.GetChild(0).position = new Vector3(transform.position.x + transform.localScale.x/2,transform.position.y, transform.position.z + transform.localScale.z/2);
-        __corners.GetChild(1).position = new Vector3(transform.position.x + transform.localScale.x/2,transform.position.y, transform.position.z - transform.localScale.z/2);
-        __corners.GetChild(2).position = new Vector3(transform.position.x - transform.localScale.x/2,transform.position.y, transform.position.z + transform.localScale.z/2);
-        __corners.GetChild(3).position = new Vector3(transform.position.x - transform.localScale.x/2,transform.position.y, transform.position.z - transform.localScale.z/2);
+        GetPoints();
     }
    
-     void LateUpdate () {
-         // this stability feature makes landing tricks alot easier, at this point i have fixed the previous problem by other means that work in all situations and so stabilization may be used as a feature for an easier mode
-         Vector3 predictedUp = Quaternion.AngleAxis(
-             __rb.angularVelocity.magnitude * Mathf.Rad2Deg * Stability / Stabilityspeed,
-             __rb.angularVelocity
-         ) * transform.up;
-         Vector3 torqueVector = Vector3.Cross(predictedUp, Vector3.up);
-         __rb.AddTorque(torqueVector * Stabilityspeed);
-         
-
-         _shipSound.pitch = Mathf.Clamp(__rb.velocity.magnitude / 
-                                        EngineAcceleration , minEngineSound, maxEngineSound);
+     private void LateUpdate ()
+     {
+         HandleStabalisation();
+         HandleAudio();
      }
     
 
-    void FixedUpdate()
+    private void FixedUpdate()
     {
-        groundLevel = false;
+        HandleInputAndHovering();
+    }
+
+    private void HandleStabalisation()
+    {
+        // this stability feature makes landing tricks alot easier, at this point i have fixed the previous problem
+        // by other means that work in all situations and so stabilization may be used as a feature for an easier mode
+        Vector3 predictedUp = Quaternion.AngleAxis(
+            _rigidbody.angularVelocity.magnitude * Mathf.Rad2Deg * stability / stabilitySpeed,
+            _rigidbody.angularVelocity
+            ) * transform.up;
+        Vector3 torqueVector = Vector3.Cross(predictedUp, Vector3.up);
+        _rigidbody.AddTorque(torqueVector * stabilitySpeed);
+    }
+
+    private void HandleAudio()
+    {
+        shipSound.pitch = Mathf.Clamp(_rigidbody.velocity.magnitude / 
+                                      engineAcceleration , minEngineSound, maxEngineSound);
+    }
+
+    private void GetPoints()
+    {
+        corners.GetChild(0).position = new Vector3(transform.position.x + transform.localScale.x/2,transform.position.y, transform.position.z + transform.localScale.z/2);
+        corners.GetChild(1).position = new Vector3(transform.position.x + transform.localScale.x/2,transform.position.y, transform.position.z - transform.localScale.z/2);
+        corners.GetChild(2).position = new Vector3(transform.position.x - transform.localScale.x/2,transform.position.y, transform.position.z + transform.localScale.z/2);
+        corners.GetChild(3).position = new Vector3(transform.position.x - transform.localScale.x/2,transform.position.y, transform.position.z - transform.localScale.z/2);
+    }
+
+    void HandleInputAndHovering()
+    {
+                _groundLevel = false;
         for (int i = 0; i < 4; i++)
         {
             RaycastHit hit;
-            if (Physics.Raycast(__corners.GetChild(i).position, transform.TransformDirection(Vector3.down), out hit, hoverDist))
+            if (Physics.Raycast(corners.GetChild(i).position, transform.TransformDirection(Vector3.down), out hit, hoverDist))
             {
-                Debug.DrawRay(__corners.GetChild(i).position, transform.TransformDirection(Vector3.down) * hit.distance, Color.yellow);
-                __upForce = (hoverDist - hit.distance) * __levelForce;
+                Debug.DrawRay(corners.GetChild(i).position, transform.TransformDirection(Vector3.down) * hit.distance, Color.yellow);
+                _upForce = (hoverDist - hit.distance) * levelForce;
                 //rb.AddForce(Vector3.up * upForce);
-                __rb.AddForceAtPosition(Vector3.up * __upForce, __corners.GetChild(i).position);
-                groundLevel = true;
+                _rigidbody.AddForceAtPosition(Vector3.up * _upForce, corners.GetChild(i).position);
+                _groundLevel = true;
             }
             else
             {
-                Debug.DrawRay(__corners.GetChild(i).position, transform.TransformDirection(Vector3.down) * hoverDist, Color.white);
+                Debug.DrawRay(corners.GetChild(i).position, transform.TransformDirection(Vector3.down) * hoverDist, Color.white);
                 
             }
         }
 
-        if (groundLevel == false)
+        if (_groundLevel == false)
         {
-            __rb.drag = 0;
-            __rb.angularDrag = 3;
-            timeUngrounded += Time.deltaTime;
-            if (timeUngrounded >= .5f)
+            _rigidbody.drag = 0;
+            _rigidbody.angularDrag = 3;
+            _timeUngrounded += Time.deltaTime;
+            if (_timeUngrounded >= .5f)
             {
-                inAir = true;
+                _inAir = true;
             }
         }
         else
         {
-            __rb.drag = 7;
-            __rb.angularDrag = 7;
+            _rigidbody.drag = 7;
+            _rigidbody.angularDrag = 7;
             if (Input.GetKey(KeyCode.W))
             {
-                __rb.AddForce(__Orientaion.forward.normalized * __speed);
+                _rigidbody.AddForce(orientation.forward.normalized * speed);
             }
             if (Input.GetKey(KeyCode.A))
             {
-                __rb.AddForce(__Orientaion.right.normalized * -__speed);
+                _rigidbody.AddForce(orientation.right.normalized * -speed);
             }
             if (Input.GetKey(KeyCode.S))
             {
-                __rb.AddForce(__Orientaion.forward.normalized * -__speed);
+                _rigidbody.AddForce(orientation.forward.normalized * -speed);
             }
             if (Input.GetKey(KeyCode.D))
             {
-                __rb.AddForce(__Orientaion.right.normalized * __speed);
+                _rigidbody.AddForce(orientation.right.normalized * speed);
             }
-            inAir = false;
+            _inAir = false;
             //__rb.AddTorque(0, Input.GetAxisRaw("Mouse X") * __rotSpeed, 0);
-            timeUngrounded = 0;
+            _timeUngrounded = 0;
         }
 
         //__rb.AddTorque(0, Input.GetAxisRaw("Mouse X") * __rotSpeed, 0);
-        __rb.AddTorque(__Orientaion.up.normalized * Input.GetAxisRaw("Mouse X") * __rotSpeed);
+        _rigidbody.AddTorque(orientation.up.normalized * Input.GetAxisRaw("Mouse X") * rotSpeed);
 
-        if (inAir)
+        if (_inAir)
         {
             
             if (Input.GetKey(KeyCode.W))
             {
-                __rb.AddTorque(__Orientaion.right.normalized * __speed);
+                _rigidbody.AddTorque(orientation.right.normalized * speed);
             }
             if (Input.GetKey(KeyCode.A))
             {
-                __rb.AddTorque(__Orientaion.forward.normalized * __speed);
+                _rigidbody.AddTorque(orientation.forward.normalized * speed);
             }
             if (Input.GetKey(KeyCode.S))
             {
-                __rb.AddTorque(__Orientaion.right.normalized * -__speed);
+                _rigidbody.AddTorque(orientation.right.normalized * -speed);
             }
             if (Input.GetKey(KeyCode.D))
             {
-                __rb.AddTorque(__Orientaion.forward.normalized * -__speed);
+                _rigidbody.AddTorque(orientation.forward.normalized * -speed);
             }
         }
     }
