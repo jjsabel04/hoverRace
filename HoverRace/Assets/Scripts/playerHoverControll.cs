f6cfd38b352c1d94fda1d9cfc9b34fd6abc6001e
jsh_studios <jasdeep.heer001@gmail.com>
2022-03-12 14:18:51 +0000

    Update playerHoverControll.cs

--- HoverRace/Assets/Scripts/playerHoverControll.cs
+++ HoverRace/Assets/Scripts/playerHoverControll.cs
@@ -200,11 +200,11 @@ public class playerHoverControll : MonoBehaviour
         }
 
 
-        if (Input.GetMouseButtonDown(1))
+        if (Input.GetMouseButton(1))
         {
             _rigidbody.drag = 15;
             _rigidbody.angularDrag = 15;
-            StartCoroutine(Boost());
+            // StartCoroutine(Boost());
             _rigidbody.AddTorque(orientation.forward.normalized * speed * boostSpeed);
         }
 
