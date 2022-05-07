4e53e1aca8b71df03379e94f2c6ecfd62c9c9f7e
jjsabel04 <100997067+jjsabel04@users.noreply.github.com>
2022-05-07 23:29:49 +0100

    good control

--- HoverRace/Assets/Scripts/playerHoverControll.cs
+++ HoverRace/Assets/Scripts/playerHoverControll.cs
@@ -27,9 +27,14 @@ public class playerHoverControll : MonoBehaviour
     [SerializeField] private float currentPoints;
 
     [SerializeField] private bool onRail;
+    [SerializeField] private bool prevOnRail;
     [SerializeField] private GameObject rail;
     [SerializeField] private GameObject nearestRail;
     [SerializeField] private float timeOnRail;
+
+    [SerializeField] private float decel;
+    [SerializeField] private bool startDecel;
+    [SerializeField] private Vector3 prevDir;
 
 
     #region Private Vars
@@ -71,8 +76,21 @@ public class playerHoverControll : MonoBehaviour
             _stabilizing = false;
         }
         HandleAudio();
-     }
+    }
      
+    void Update()
+    {
+        
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            Debug.Log("a");
+            if (!onRail)
+            {
+                Debug.Log("b");
+                prevOnRail = false;
+            }
+        }
+    }
 
     void FixedUpdate()
     {
@@ -104,51 +122,110 @@ public class playerHoverControll : MonoBehaviour
         corners.GetChild(2).position = new Vector3(transform.position.x - transform.localScale.x/2,transform.position.y, transform.position.z + transform.localScale.z/2);
         corners.GetChild(3).position = new Vector3(transform.position.x - transform.localScale.x/2,transform.position.y, transform.position.z - transform.localScale.z/2);
     }
+    void OnDrawGizmosSelected()
+    {
+        // Draw a yellow sphere at the transform's position
+        Gizmos.color = Color.yellow;
+        if(nearestRail!= null)
+        {
+            Gizmos.DrawSphere(nearestRail.transform.GetComponent<Collider>().ClosestPointOnBounds(transform.position), .1f);
+        }
+    }
+
+    private Vector3 OrientTorque(Vector3 torque)
+    {
+        // Quaternion's Euler conversion results in (0-360)
+        // For torque, we need -180 to 180.
+ 
+        return new Vector3
+        (
+            torque.x > 180f ? 180f - torque.x : torque.x,
+            torque.y > 180f ? 180f - torque.y : torque.y,
+            torque.z > 180f ? 180f - torque.z : torque.z
+        );
+    }
 
     void HandleInputAndHovering()
     {
         nearestRail = null;
         _groundLevel = false;
         _inAir = false;
-        onRail = false;
         rotDecrease = 1;
+        onRail = false;
 
         if(Input.GetKey(KeyCode.Space))
         {
-            Collider[] nearHover = Physics.OverlapBox(transform.position, transform.localScale*3f, transform.rotation);
+            Collider[] nearHover = Physics.OverlapBox(transform.position, transform.localScale*2f, transform.rotation);
+            if (nearHover.Length > 1)
+            {
+                for (int i = 0; i < nearHover.Length; i++)
+                {
+                    //Debug.Log(nearHover[i].name);
+                    if (nearHover[i].tag == "rail")
+                    {
+                        if (nearestRail == null || Vector3.Distance(transform.position,nearHover[i].transform.position) < Vector3.Distance(transform.position,nearestRail.transform.position))
+                        {
+                            nearestRail = nearHover[i].gameObject;
+                        }
+                    }
+                }
+                
+                if(nearestRail != null)
+                {
+                    Vector3 closestPoint = nearestRail.transform.GetComponent<Collider>().ClosestPoint(transform.position);
+                    if (closestPoint.y - 0.01f < transform.position.y)
+                    {
+                        if ((transform.eulerAngles.x > 290 || transform.eulerAngles.x < 70) && (transform.eulerAngles.z > 290 || transform.eulerAngles.z < 70))
+                        {
+                            Vector3 directionVector = (closestPoint - transform.position).normalized;
+                            _rigidbody.AddForce (directionVector * 1500 * (Vector3.Distance(transform.position,closestPoint) + .3f));
+                            if (prevOnRail == false)
+                            {
+                                transform.position = closestPoint;
+                                timeOnRail = currentPoints;
+                                prevOnRail = true;
+                            }
+                        }
+                    }
+                }
+            }
+            nearHover = Physics.OverlapBox(transform.position, transform.localScale*1.5f, transform.rotation);
             if (nearHover.Length > 1)
             {
                 for (int i = 0; i < nearHover.Length; i++)
                 {
                     if (nearHover[i].tag == "rail")
                     {
-                        nearestRail = nearHover[i].gameObject;
-                    }
-                }
-                if(nearestRail != null)
-                {
-                    Vector3 closestPoint = nearestRail.transform.GetComponent<Collider>().ClosestPointOnBounds(transform.position);
-                    Vector3 directionVector = (closestPoint - transform.position).normalized;
-                    _rigidbody.AddForce (directionVector * 1000);
-                    //transform.position = closestPoint;
-
-
-
-                    nearHover = Physics.OverlapBox(transform.position, transform.localScale*1.5f, transform.rotation);
-                    if (nearHover.Length > 1)
-                    {
-                        for (int i = 0; i < nearHover.Length; i++)
+                        onRail = true;
+                        rail = nearHover[i].gameObject;
+                        _rigidbody.drag = 10;
+                        _rigidbody.angularDrag = 20;
+                        rotDecrease = 40;
+                        timeOnRail += Time.deltaTime * 5;
+                        currentPoints = Mathf.FloorToInt(timeOnRail);
+                        
+                        if(Mathf.Abs(_rigidbody.velocity.x) > 1f || Mathf.Abs(_rigidbody.velocity.z) > 1f)
                         {
-                            if (nearHover[i].tag == "rail")
-                            {
-                                onRail = true;
-                                rail = nearHover[i].gameObject;
-                                _rigidbody.angularDrag = 20;
-                                rotDecrease = 40;
-                                timeOnRail += Time.deltaTime * 5;
-                                currentPoints = Mathf.FloorToInt(timeOnRail);
-                            }
+                            _rigidbody.velocity = _rigidbody.velocity.normalized * 17f * (currentPoints / 65 + 1);
                         }
+                        /*
+                        //Transform targetRot = null;
+                        orientation.rotation = Quaternion.LookRotation(_rigidbody.velocity, Vector3.up);
+                        // Determine Quaternion 'difference'
+                        // The conversion to euler demands we check each axis
+                        Vector3 torqueF = OrientTorque(Quaternion.FromToRotation(this.transform.forward, orientation.transform.forward).eulerAngles);
+                        Vector3 torqueR = OrientTorque(Quaternion.FromToRotation(this.transform.right, orientation.transform.right).eulerAngles);
+                        Vector3 torqueU = OrientTorque(Quaternion.FromToRotation(this.transform.up, orientation.transform.up).eulerAngles);
+                
+                        float magF = torqueF.magnitude;
+                        float magR = torqueR.magnitude;
+                        float magU = torqueU.magnitude;
+                
+                        // Here we pick the axis with the least amount of rotation to use as our torque.
+                        Vector3 torque = magF < magR ? (magF < magU ? torqueF : torqueU) : (magR < magU ? torqueR : torqueU);
+                        
+                        _rigidbody.AddTorque(torque * Time.fixedDeltaTime * .0001f);
+                        */
                     }
                 }
             }
@@ -157,15 +234,21 @@ public class playerHoverControll : MonoBehaviour
         {
             timeOnRail = 0;
         }
-
         if(!Input.GetKey(KeyCode.Space))
         {
             if (jumpForce > 1)
             {
                 jumpForce -= 0.1f;
+                if(prevOnRail == true)
+                {
+                    Debug.Log(jumpForce);
+                    _rigidbody.AddForce(transform.up * 200 * jumpForce);
+                    jumpForce -= 0.03f;
+                }
             }
             else
             {
+                prevOnRail = false;
                 jumpForce = 1;
             }
         }
@@ -177,9 +260,9 @@ public class playerHoverControll : MonoBehaviour
         for (int i = 0; i < 4; i++)
         {
             RaycastHit hit;
-            if (Physics.Raycast(corners.GetChild(i).position, transform.TransformDirection(Vector3.down), out hit, hoverDist + 3))
+            if (Physics.Raycast(corners.GetChild(i).position, transform.TransformDirection(Vector3.down), out hit, hoverDist + 3, 7))
             {
-                if(hit.distance < hoverDist && !Input.GetKey(KeyCode.Space) && hit.transform.gameObject.tag != "rail")
+                if(hit.distance < hoverDist && !Input.GetKey(KeyCode.Space))
                 {
                     Debug.DrawRay(corners.GetChild(i).position, transform.TransformDirection(Vector3.down) * hit.distance, Color.yellow);
                     _upForce = (hoverDist - hit.distance) * levelForce;
@@ -222,6 +305,30 @@ public class playerHoverControll : MonoBehaviour
                 MovementLR += transform.right;
             }
             
+            ////////////////// deceleration ////////////////////
+            if (!Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.D))
+            {
+                if (startDecel == false)
+                {
+                    decel = speed;
+                    startDecel = true;
+                }
+                decel -= Time.deltaTime * 10000;
+                if (decel < 0)
+                {
+                    decel = 0;
+                }
+                Debug.Log(decel);
+                _rigidbody.AddForce(prevDir * decel);
+            }
+            else
+            {
+                prevDir = (MovementFB + MovementLR).normalized;
+                startDecel = false;
+                decel = 1;
+            }
+            /////////////////////////////////////////////////
+
             if (currentPoints > 0)
             {
                 //_rigidbody.AddForce();
@@ -234,13 +341,14 @@ public class playerHoverControll : MonoBehaviour
 
 
 
-            _rigidbody.AddForce((MovementFB + MovementLR).normalized * speed * (currentPoints / 65 + 1));
+            _rigidbody.AddForce((MovementFB + MovementLR).normalized * speed * decel * (currentPoints / 65 + 1));
             
             
             
         }
         else if(!onRail)
         {
+            //prevOnRail = false;
             _rigidbody.drag = 0;
             _rigidbody.angularDrag = 3;
         }
@@ -300,14 +408,16 @@ public class playerHoverControll : MonoBehaviour
         {
             currentPoints = 85;
         }
-        Debug.Log(currentPoints);
+        //Debug.Log(currentPoints);
     }
 
     void OnCollisionEnter(Collision other)
     {
-        if(!_groundLevel)
+        if(!_groundLevel && other.gameObject.tag != "rail")
         {
+            Debug.Log(other.gameObject.tag);
             currentPoints = 0;
         }
+        //Debug.Log(currentPoints);
     }
 }
